04672c8e7a7b5cf3252fbbc4cda257a8482399bd
EjieBoy09 <150094268+EjieBoy09@users.noreply.github.com>
2026-03-23 16:51:18 +0800

    fix routing for github pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import StaticPage from "./pages/static";
 import LandingPage from "./pages/landing";
 import Header from "./components/header";
